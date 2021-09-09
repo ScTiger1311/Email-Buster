c402ced4b4123e82ce5dd74c5243a56e2f9beee9
ScTiger1311 <winsoftheepic@gmail.com>
2021-09-09 11:46:04 -0700

    Added end of day events support. Only 1 event so far, but it tutorializes heading home for the day.

--- src/scenes/Pixelroom.js
+++ src/scenes/Pixelroom.js
@@ -7,6 +7,7 @@ class Pixelroom extends Phaser.Scene {
     preload() {
         this.load.image("PixelroomBG", "./assets/single_sprites/Pixel_Room_BG.png");
         this.load.image("Figure", "./assets/single_sprites/Figure.png");
+        this.load.image("Figure_Pink", "./assets/single_sprites/Figure_Pink.png");
         this.load.image("Arrow", "./assets/single_sprites/Arrow.png");
         this.load.atlas("button", "./assets/spritesheets/button_spritesheet.png", "./assets/spritesheets/button_spritesheet.json"); //this one is used as a test button
     }
@@ -18,6 +19,7 @@ class Pixelroom extends Phaser.Scene {
         this.movespeed = 10; //in pixels per 1/60th of a second
         this.dayOver = false; //has the player checked their emails for today? true/false/"inShopMenu"
         this.dayNumber = 0; //0 is the first day, and includes the whole tutorial
+        this.justSwitchedFromPlayScene = false; 
         this.floorheight = 734;
         this.playerPaused = false;
         this.bgSprite = this.add.sprite(0, 0, "PixelroomBG").setOrigin(0, 0);
@@ -26,12 +28,28 @@ class Pixelroom extends Phaser.Scene {
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.tutorialText = this.add.text(600, 230, "Welcome to Email Buster!\nUse the left/right arrow keys to move." ,
+        { 
+            fontFamily: 'Tahoma, "Goudy Bookletter 1911", Times, serif',
+            fontSize: "40px",
+            color: "#000000",
+            align: "center",
+        }).setOrigin(0.5,0);
         this.movementArrowLeft = this.add.sprite(this.player.x - 90, this.player.y + 10 - this.player.displayHeight, "Arrow").setOrigin(0,0).setAngle(-90).setScale(this.playerScale/2);
         this.movementArrowRight = this.add.sprite(this.player.x + 90, this.player.y + 10 - this.player.displayHeight, "Arrow").setOrigin(1,1).setAngle(90).setScale(this.playerScale/2);
     }
 
     update(time, delta) {
         let deltaMultiplier = (delta / 16.66667); //for refresh rate indepence
+
+        if(this.justSwitchedFromPlayScene === true) //code in here runs once whenever you come from the play scene.
+        {
+            this.justSwitchedFromPlayScene = false;
+            if(this.dayNumber === 0)
+            {
+                this.delayedCallTimer = this.time.delayedCall(750, this.endOfDayEvent, [0], this);
+            }
+        }
 
         //handles the very basic and limited movement
         if(this.playerPaused === false)
@@ -45,6 +63,8 @@ class Pixelroom extends Phaser.Scene {
                     this.movementArrowLeft = 0;
                     this.movementArrowRight.destroy();
                     this.movementArrowRight = 0;
+                    this.tutorialText.destroy();
+                    this.tutorialText = 0;
                 }
             }
             if(keyRIGHT.isDown === true && this.player.x < game.config.width - 58 - this.player.width * this.playerScale)
@@ -60,6 +80,7 @@ class Pixelroom extends Phaser.Scene {
                 if(keyUP.isDown == true)
                 {
                     this.dayOver = true;
+                    this.justSwitchedFromPlayScene = true; //switches this to true, the immediately sleeps the scene. Upon switching back the code will execute.
                     this.scene.switch("Play");
                 }
             }
@@ -90,7 +111,14 @@ class Pixelroom extends Phaser.Scene {
         {
             this.cameras.main.fadeFrom(200, 20, 20, 20, true);
             this.shopBG = this.add.rectangle(0, 0, 1200, 900, 0x202020).setOrigin(0,0);
-            this.nextDayButton = new Button (this, "button", 500, 700, this.closeShopMenu)
+            this.nextDayButton = new Button (this, "button", 575, 620, this.closeShopMenu)
+            this.dayOverText = this.add.text(600, 230, "You head home after a long day of work.\nYou wake the next day feeling re-engergized and ready for more.\n\n\n\n\n\nContinue" ,
+            { 
+                fontFamily: 'Tahoma, "Goudy Bookletter 1911", Times, serif',
+                fontSize: "40px",
+                color: "#ffffff",
+                align: "center",
+            }).setOrigin(0.5,0);
         }
     }
 
@@ -98,9 +126,32 @@ class Pixelroom extends Phaser.Scene {
     {
         this.nextDayButton._removeButton();
         this.shopBG.destroy();
+        this.dayOverText.destroy();
         this.cameras.main.fadeFrom(200, 20, 20, 20, true);
         this.dayNumber++;
         this.dayOver = false;
         this.playerPaused = false;
+        this.removeEndOfDayEvent();
+    }
+
+    endOfDayEvent(eventNumber) //runs the event, under the event number specified
+    {
+        if(eventNumber === 0) //the tutorial event, mainly used to prompt the player into leaving the room
+        {
+            this.tutorialText = this.add.text(600, 230, "Nice job today! Looks like your shift is over.\nYou can head home now." ,
+            { 
+                fontFamily: 'Tahoma, "Goudy Bookletter 1911", Times, serif',
+                fontSize: "40px",
+                color: "#000000",
+                align: "center",
+            }).setOrigin(0.5,0);
+            this.npcSprite = this.add.sprite(900, 734, "Figure_Pink").setOrigin(0, 1).setScale(this.playerScale);
+        }
+    }
+
+    removeEndOfDayEvent() //just removes all of the end of day event related stuff
+    {
+        this.tutorialText.destroy();
+        this.npcSprite.destroy();
     }
 }
